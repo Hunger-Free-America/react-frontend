d09c368d077f57fcf068c044d9fb1c0bcea3d822
Max Stuart <maxastuart@gmail.com>
2020-07-12 14:42:33 -0700

    added svgs and changed imports

--- src/components/common/Pin.tsx
+++ src/components/common/Pin.tsx
@@ -1,37 +1,36 @@
 import React from "react"
 
 import { PlaceIcon } from "./PlaceIcon"
-import govPin from "../../assets/pins/govPin.png"
 import { Icon as PinProps } from "./interface/Icon"
-import defaultPin from "../../assets/pins/default.png"
-import govPinRed from "../../assets/pins/govPinRed.png"
-import schoolPin from "../../assets/pins/schoolPin.png"
-import worshipPin from "../../assets/pins/worshipPin.png"
-import defaultPinRed from "../../assets/pins/defaultRed.png"
-import schoolPinRed from "../../assets/pins/schoolPinRed.png"
-import worshipPinRed from "../../assets/pins/worshipPinRed.png"
+
+import genericPin from "../../assets/genericPin.svg"
+import genericPinSelected from "../../assets/genericPinSelected.svg"
+import govPin from "../../assets/govPin.svg"
+import govPinSelected from "../../assets/govPinSelected.svg"
+import schoolPin from "../../assets/schoolPin.svg"
+import schoolPinSelected from "../../assets/schoolPinSelected.svg"
+import worshipPin from "../../assets/worshipPin.svg"
+import worshipPinSelected from "../../assets/worshipPinSelected.svg"
 
 export const Pin = ({ allGmapsTypes, selectedPin }: PinProps) => {
   switch (allGmapsTypes ? allGmapsTypes[0] : "") {
-    case "school":
-    case "primary_school":
+    case "school" || "primary_school":
       return (
         <PlaceIcon
           width="30px"
           alt="School"
           icon={schoolPin}
-          redIcon={schoolPinRed}
+          redIcon={schoolPinSelected}
           selectedPin={selectedPin}
         />
       )
-    case "church":
-    case "place_of_worship":
+    case "church" || "place_of_worship":
       return (
         <PlaceIcon
           width="30px"
           icon={worshipPin}
           alt="Place of worship"
-          redIcon={worshipPinRed}
+          redIcon={worshipPinSelected}
           selectedPin={selectedPin}
         />
       )
@@ -41,7 +40,7 @@ export const Pin = ({ allGmapsTypes, selectedPin }: PinProps) => {
           width="30px"
           icon={govPin}
           alt="Government"
-          redIcon={govPinRed}
+          redIcon={govPinSelected}
           selectedPin={selectedPin}
         />
       )
@@ -49,9 +48,9 @@ export const Pin = ({ allGmapsTypes, selectedPin }: PinProps) => {
       return (
         <PlaceIcon
           width="30px"
-          alt="Default"
-          icon={defaultPin}
-          redIcon={defaultPinRed}
+          alt="Generic Charity Site"
+          icon={genericPin}
+          redIcon={genericPinSelected}
           selectedPin={selectedPin}
         />
       )
